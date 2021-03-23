b9af8753669a18316603b8b7d95770976c96273c
Rodrigo Hernandez <electro001_@hotmail.com>
2021-03-23 02:37:25 -0600

    init app sturcture and reestructuring folders

---
+++ NexusApp/screens/RegisterScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RegisterScreen = () => {
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Register Screen</Text>
+  </View>;
+};
+
+export default RegisterScreen;
